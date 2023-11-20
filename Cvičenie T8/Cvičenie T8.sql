9815c59204b787dd238d5d9d679cf96d1279c2a3
TobMit <80719513+tobmit@users.noreply.github.com>
2023-11-20 09:13:00 +0100

    Koniec hodiny

--- Cvičenie T8/Cvičenie T8.sql
+++ Cvičenie T8/Cvičenie T8.sql
@@ -63,4 +63,47 @@ select lpad(' ', 2*level) || name || ' ' || surname
     from person_rec
     connect by prior person_id = mother_id; -- poradie atribútov je veľmi dôležité ak by som to vymenil tak by rodokmeň išiel od detí k rodičom
 
--- todo zobrať našú fakutlu, každá fakulta má prodekana, dekan má nad sebo prodekana a takto spraviť podobný výpis za 2 bonusové body...
+-- todo zobrať našú fakutlu, každá fakulta má prodekana, dekan má nad sebo prodekana a takto spraviť podobný výpis za 2 bonusové body do nedele
+
+-- tabuľka vzťahov na oddeleniach
+create table t_zam
+(rod_cislo char(11) primary key,
+ login varchar(20) not null unique,
+ meno varchar(20),
+ priezvisko varchar(20),
+ id_oddelenie integer not null
+);
+
+create table t_oddelenie
+(id_oddelenie integer primary key,
+ nazov varchar(20) not null,
+ veduci varchar(20) not null
+);
+
+alter table t_zam add foreign key (id_oddelenie)
+    references t_oddelenie(id_oddelenie) deferrable ;
+
+alter table t_oddelenie add foreign key (veduci)
+    references t_zam(login) deferrable ;
+
+-- keď by som to takto chcel vložiť tak nemôžem, lebo by mi to kontrola nedovolila
+-- musíme povoliť rôzne vkladanie, čo znamená odložiť kontrolu referencií na koniec
+select constraint_name
+    from USER_CONSTRAINTS
+        where TABLE_NAME = 'T_ODDELENIE'
+            and CONSTRAINT_TYPE = 'R';
+alter table t_oddelenie drop constraint SYS_C00725153;
+
+alter session set constraint = deferred;
+
+insert into t_zam values ('1234345', 'tobias','Tobias', 'Mitala', 1);
+insert into t_oddelenie values (1, 'KI', 'tobias');
+
+insert into t_zam values ('12343', 'tobias2','Tobias', 'Mitala', 2);
+insert into t_zam values ('123434', 'tobias3','Tobias', 'Mitala', 3);
+insert into t_oddelenie values (2, 'KMMOA', 'tobias2');
+
+commit; -- toto keď by som commitol tak to neprejde (v reale da rollback) lebo je to zlá transakcia a tá nemôže prejsť
+
+-- pripojenie sa na iný server, potrebujem databázoví link
+-- ak sa chcem odkázať na tabuľku v nejakom inom servery tak použijem použivateľ.os_udaje@db_link
